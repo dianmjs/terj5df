4a5ea47d77d7c460f871627d6828e184ce8acae6
dianmjs <dianamjs69@gmail.com>
2020-05-15 14:23:39 -0500

    form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      guests: [],
+      firstName: "",
+      secondName: "",
+    };
+  }
+
+  updateGuessFirstName(e) {
+    this.setState({
+      firstName: e.target.value,
+    });
+  }
+
+  updateGuessSecondName(e) {
+    this.setState({
+      secondName: e.target.value,
+    });
+  }
+
+  createGuess(e) {
+    e.preventDefault();
+    let oldGuests = this.state.guests;
+    let newGuess = {
+      id: this.state.guests.length + 1,
+      firstName: this.state.firstName,
+      secondName: this.state.secondName,
+    };
+    this.setState({
+      guests: [...oldGuests, newGuess],
+      firstName: "",
+      secondName: "",
+    });
+  }
+
   render() {
     return (
-      <div className="container">
+      <div className="container" id="root">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.createGuess.bind(this)}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input
+                  value={this.state.firstName}
+                  onChange={this.updateGuessFirstName.bind(this)}
+                  type="text"
+                  className="form-control"
+                  name="first-name"
+                />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input
+                  value={this.state.secondName}
+                  onChange={this.updateGuessSecondName.bind(this)}
+                  type="text"
+                  className="form-control"
+                  name="last-name"
+                />
               </div>
 
               <div className="action">
-                <button type="submit" className="btn btn-primary">Agregar Invitado</button>
+                <button type="submit" className="btn btn-primary">
+                  Agregar Invitado
+                </button>
               </div>
             </form>
 
@@ -28,18 +79,20 @@ class App extends Component {
                   <th>Nombre</th>
                   <th>Apellido</th>
                 </tr>
+                {this.state.guests.map((guess, index) => (
+                  <tr key={guess.id}>
+                    <td>{guess.firstName}</td>
+                    <td>{guess.secondName}</td>
+                  </tr>
+                ))}
               </thead>
-              <tbody>
-
-              </tbody>
+              <tbody></tbody>
             </table>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
-
-
+export default App;
